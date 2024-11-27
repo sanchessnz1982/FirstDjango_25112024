c9d859fa6b030aa7d5338e6bedfd9e22eb417665
sanches <sanchessnz@gmail.com>
2024-11-28 02:06:42 +0500

    MainApp: add model Color and relationship MM with Item in views

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from pip._vendor.rich.color import Color
+
 
 # Create your models here.
 
 class Item(models.Model):
-    name  = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     description = models.TextField(default="")
+    colors = models.ManyToManyField(to="Color")
 
     def __str__(self):
         return  self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=16, unique=True)
